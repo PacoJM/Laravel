70684fad11750fb2482e9b06432ad511545f3c5b
PacoJM <paco_j_m_14@hotmail.com>
2022-10-20 22:36:45 +0200

    Cambio ruta dashboard por admin

--- login-onepage-laravel/routes/web.php
+++ login-onepage-laravel/routes/web.php
@@ -34,12 +34,10 @@ Route::get('photos', function () {
     ]);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
 });
